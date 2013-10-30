3935ddaecf541061c0d4aab91e4fe94bf0c1b7e0
jcl <jcl@IIT53417N.info-ag.de>
2013-10-30 11:37:44 +0100

    Einfügen der leeren DeleteEventAction Klasse

---
+++ EventYourLife/src/de/nordakademie/evyoli/event/action/DeleteEventAction.java
@@ -0,0 +1,13 @@
+package de.nordakademie.evyoli.event.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class DeleteEventAction implements Action {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
